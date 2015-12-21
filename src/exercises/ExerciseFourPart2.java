7e150ac204173eedf120e71d1c209cc65fdc347e
ArielAlvarado <alvarado.ariel.contact@gmail.com>
2015-12-21 14:40:24 -0400

    wohooooo

--- src/exercises/ExerciseFourPart2.java
+++ src/exercises/ExerciseFourPart2.java
@@ -13,29 +13,14 @@ import java.util.Set;
 
 /**
  *
- * --- Day 4: The Ideal Stocking Stuffer ---
+ * Now find one that starts with six zeroes.
  *
- * Santa needs help mining some AdventCoins (very similar to bitcoins) to use as
- * gifts for all the economically forward-thinking little girls and boys.
  *
- * To do this, he needs to find MD5 hashes which, in hexadecimal, start with at
- * least five zeroes. The input to the MD5 hash is some secret key (your puzzle
- * input, given below) followed by a number in decimal. To mine AdventCoins, you
- * must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...)
- * that produces such a hash.
- *
- * For example:
- *
- * If your secret key is abcdef, the answer is 609043, because the MD5 hash of
- * abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest
- * such number to do so. If your secret key is pqrstuv, the lowest number it
- * combines with to make an MD5 hash starting with five zeroes is 1048970; that
- * is, the MD5 hash of pqrstuv1048970 looks like 000006136ef.... Your puzzle
- * input is bgvyzdsv.
+ * solution 1038736
  *
  * @author aalvarado
  */
-public class ExerciseFour {
+public class ExerciseFourPart2 {
 
     public static String toHex(String arg) {
         return String.format("%040x", new BigInteger(1, arg.getBytes(StandardCharsets.UTF_8)));
@@ -57,7 +42,7 @@ public class ExerciseFour {
             }
             for (number = 0; number <= 9999999; number++) {
                 hashedString = Hash.getHash(data + number, "MD5");
-                if (hashedString.startsWith("00000")) {
+                if (hashedString.startsWith("000000")) {
                     break;
                 }
             }
